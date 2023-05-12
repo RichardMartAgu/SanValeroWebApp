5fd3a92b96217a4060f641271182c6176af972d1
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-05-12 18:04:24 +0200

    Solucionar DAO mayusculas

--- src/main/java/com/Svalero/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.Svalero.util;
-
-public class Constants {
-
-  public static String DATABASE_URL = "jdbc:mysql://localhost:3306/sanvalero";
-  public static String DATABASE_USERNAME = "SanValeroAdmin";
-  public static String DATABASE_PASSWORD = "1234";
-}
--- src/main/java/com/sanValero/DAO/StudentDAO.java
+++ src/main/java/com/sanValero/DAO/StudentDAO.java
@@ -1,4 +1,4 @@
-package com.sanValero.DAO;
+package com.sanValero.dao;
 
 import com.sanValero.Domain.Student;
 
